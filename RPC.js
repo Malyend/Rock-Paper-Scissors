91d31de622eb57ce7f635bd32f538a0896f4c973
malyend <191012129+Malyend@users.noreply.github.com.>
2026-03-14 22:35:36 +0000

    Ummmm
    i may have forgotten to commit regularly.
    I struggled with this for the better
    half of a day though.
    
    Learnt alot. Also need to learn alot more.
    I want to style it and then append a paragraph or
    so i can create a paragraph to show the score
    board through the GUI.
    but yeah, main stuff is done.
    
    Had to delete all of it once. added the while loop
    later on after i had finished everything.
    
    Everything's hunky dory, everything but that one
    TypeError i still need to debug. Gosh.

---
+++ RPC.js
@@ -0,0 +1,103 @@
+// Take 2, Lets do this
+
+// Once complete create a playGame function
+    // add playRound inside of this function
+    // call it five times. So that you play five rounds
+    let humanScore = 0;
+    let computerScore = 0;
+    function playGame(){
+        
+
+        while( humanScore < 5 && computerScore < 5){
+            playRound()
+        }
+
+    }
+// create var for humanScore and computerScore
+    // values = 0
+
+
+// Write the logic for computer choice
+// computer will randomly return "Rock", "Paper" or "scissors"
+function getComputerChoice(){
+
+    let mathRandy = Math.random();
+    let compChoice = undefined;
+
+    if (mathRandy < 0.33) {
+        compChoice = "rock";
+    } else if (mathRandy < 0.66) {
+        compChoice = "paper";
+    } else {
+        compChoice = "scissors";
+    }
+
+    return compChoice;
+}
+
+// write the logic for User Choice
+// get user input
+ // prompt user.
+
+ function getUserChoice(){
+    let userChoice = prompt("rock, paper, scissors? ^_^ =: ").toLowerCase();
+
+    while (userChoice !== "rock"
+         && userChoice !== "paper" 
+         && userChoice !== "scissors")
+        {
+            alert("That's not it! Do it properly!")
+            userChoice = prompt("rock, paper, scissors? ^_^ =: ").toLowerCase();
+        }
+    return userChoice;
+ }
+
+// write logic to play a single round
+    // make humanChoice Case sensitive 
+    // console.log a string value that defines who the winner is and why. 
+    // increment the humanScore or computerScore based on the round winner
+
+    function playRound(humanChoice, computerChoice){
+
+         humanChoice = getUserChoice();
+         computerChoice = getComputerChoice();
+
+         console.log(humanChoice, computerChoice);
+
+         switch(true){
+            case (humanChoice == computerChoice):
+                alert("draw!")
+                break;
+            case (humanChoice == "rock" && computerChoice == "paper"):
+                alert("Oh shucks playa! Ya lost. Paper beats rock")
+                computerScore++;
+                break;
+            case (humanChoice == "scissors" && computerChoice == "rock"):
+                alert("well.. that's a fail, you've been smashed. rock pummels scissors.")
+                computerScore++;
+                break;
+            case (humanChoice == "paper" && computerChoice == "scissors"):
+                alert("Oof- that stings, you've failed. scissors cuts paper.")
+                computerScore++;
+                break;
+            case (computerChoice == "rock" && humanChoice == "paper"):
+                alert("Way to go! you've won! the rock has been restrained")
+                humanScore++;
+                break;
+            case (computerChoice == "scissors" && humanChoice == "rock"):
+                alert("AND THE WINNER IS--- YOU! scissors have been OBLITORATED")
+                humanScore++;
+                break;
+            case (computerChoice == "paper" && humanChoice == "scissors"):
+                alert("feeling a lil bit stabby eh? You win! scissors cut through paper!")
+                humanScore++;
+                break;
+            default :
+            alert("wait... Who brought a gun in here? AHH- ERROR\\404¬\\")
+            break;
+         }
+
+        console.log(humanScore + ":" + computerScore)
+        return playRound
+    }
+
